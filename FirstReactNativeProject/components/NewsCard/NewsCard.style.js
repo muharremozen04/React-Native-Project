87c5f482e49455ee1877561e7af43d41086a3bb9
Muharrem Özen <60547236+muharremozen04@users.noreply.github.com>
2022-03-08 17:21:39 +0300

    React Native Project
    Patika dev kursunda yaptığımız ilk React Native projesi.

---
+++ FirstReactNativeProject/components/NewsCard/NewsCard.style.js
@@ -0,0 +1,20 @@
+import {Dimensions, StyleSheet } from "react-native";
+export default StyleSheet.create({
+
+    container: {
+        backgroundColor:'white',
+        margin: 10,
+        borderRadius: 10,
+
+    },
+    image: {height: Dimensions.get('window').height / 4,
+     borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,},
+    title: {fontWeight:'bold',
+            fontSize: 20,},
+    description: {
+        marginTop: 3,
+    },
+}
+
+)
